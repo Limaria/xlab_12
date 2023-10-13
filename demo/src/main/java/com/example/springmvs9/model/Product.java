9314cce615cd372097b1e10dbb7b8cae0ac52e61
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2023-10-13 19:05:23 +0500

    Initial commit

---
+++ demo/src/main/java/com/example/springmvs9/model/Product.java
@@ -0,0 +1,34 @@
+package com.example.springmvs9.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private boolean bought;
+
+    public Product(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isBought(){
+        return bought;
+    }
+    public void setBought(boolean bought) {
+        this.bought = bought;
+    }
+}
